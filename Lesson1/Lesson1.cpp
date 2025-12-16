6dd1448e0e1ba2791d8889455860efd69a979ac6
Alex <seperaconer@gmail.com>
2025-12-16 20:37:32 +0700

    21111211112220

--- Lesson1/Lesson1.cpp
+++ Lesson1/Lesson1.cpp
@@ -1,4 +1,4 @@
-﻿//#include <iostream>
+﻿#include <iostream>
 #include <time.h>
 #include <cstring>
 //#include "main2.h"
@@ -7,11 +7,12 @@
 //#include "uptack.h"
 //#include "uptack2.h"
 
-#include "glava2.h"
+//#include "glava2.h"
 #include "glava4.h"
-#include "glava12.h"
-#include "operator6.h"
-#include "operator6-1.h"
+//#include "glava7.h"
+//#include "glava12.h"
+//#include "operator6.h"
+//#include "operator6-1.h"
 using namespace std;
 
 
@@ -30,8 +31,18 @@ int main(){
      //main012();
      //main013();
      //main014();
-    main015();
-    //main1200();
+    //main015();
+    //main016();
+    //main018();  //домашнее
+    //main019();
+    //main020();
+    main59();
+    //main022();
+   // main023();
+   // main0231();
+    //main024();
+    //main701();
+    //main702();
     //x6();
     //x7();
 
